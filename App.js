b9055fd05ee44241617daba9eddec2d82aa53874
Kengo Johira <kengo.johira@speee.jp>
2018-03-02 22:04:19 +0900

    全角カナを半角カナに変換

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, TextInput, Text } from 'react-native';
+import moji from 'moji';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '変換したい文字を入力してください',
+      kana: ''
+    };
+  }
+
+  onPress = () => {
+    this.setState({kana: moji(this.state.text).convert('ZK', 'HK').toString()})
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput
+          style={{height: 80, borderColor: 'gray', borderWidth: 1}}
+          multiline={true}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text} />
+        <Button
+          title='カナ変換'
+          onPress={this.onPress} />
+        <Text>{this.state.kana}</Text>
       </View>
     );
   }
@@ -15,9 +34,6 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20
   },
 });
